5cbaf48ddd8e74555f3057879dbd551b94bc20c8
nataliajagus <natalia.jagus@gmail.com>
2019-12-16 12:50:40 +0100

    add RWD

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ let websiteCardHeaders = document.querySelectorAll('.website-card-header');
 websiteCardHeaders.forEach(header => header.addEventListener("click", (e) => {
     let websiteCard = header.closest(".website-card");
 
-    if(websiteCard.offsetHeight <= 86) {
+    if(websiteCard.offsetHeight <= 120) {
         websiteCard.style.maxHeight = "1000px";
+    } else if(window.innerWidth <= 768) {
+        websiteCard.style.maxHeight = "110px";
     } else {
         websiteCard.style.maxHeight = "86px";
     }
 }))
+
+
+// SHORTEN LINKS
+
+let links = document.querySelectorAll(".website-card a");
+
+links.forEach(link => link.innerText = link.innerText.substring(0,30) + "...");
+  
