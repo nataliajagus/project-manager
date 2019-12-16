98577f0ceb7be5ffc0c0dd00153b322db60ddcae
nataliajagus <natalia.jagus@gmail.com>
2019-12-16 14:38:02 +0100

    fix collapse on mobile

--- js/main.js
+++ js/main.js
@@ -4,19 +4,57 @@ let websiteCardHeaders = document.querySelectorAll('.website-card-header');
 websiteCardHeaders.forEach(header => header.addEventListener("click", (e) => {
     let websiteCard = header.closest(".website-card");
 
-    if(websiteCard.offsetHeight <= 140) {
-        websiteCard.style.maxHeight = "3000px";
-    } else if(window.innerWidth <= 768) {
-        websiteCard.style.maxHeight = "140px";
-    } else {
+    if(websiteCard.offsetHeight < 450) {
+        websiteCard.style.maxHeight = "450px";
+        websiteCard.style.overflowY = "scroll";
+    } else if(window.innerWidth >= 427 && window.innerWidth <= 768) {
+        websiteCard.style.maxHeight = "170px";
+        websiteCard.style.overflow = "hidden";
+    } else if(window.innerWidth > 768) {
         websiteCard.style.maxHeight = "86px";
+        websiteCard.style.overflow = "hidden";
+    } else {
+        websiteCard.style.maxHeight = "140px";
+        websiteCard.style.overflow = "hidden";
     }
+
 }))
+
+
 
 
 // SHORTEN LINKS
 
 let links = document.querySelectorAll(".website-card a");
 
-links.forEach(link => link.innerText = link.innerText.substring(0,30) + "...");
+if(window.innerWidth < 600) {
+    links.forEach(link => link.innerText = link.innerText.substring(0,40) + "...");
+}
+
+if(window.innerWidth < 420) {
+    links.forEach(link => link.innerText = link.innerText.substring(0,30) + "...");
+}
+
+if(window.innerWidth < 330) {
+    links.forEach(link => link.innerText = link.innerText.substring(0,20) + "...");
+}
+
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth < 600) {
+        links.forEach(link => link.innerText = link.innerText.substring(0,40) + "...");
+    }
+
+    if(window.innerWidth < 420) {
+        links.forEach(link => link.innerText = link.innerText.substring(0,30) + "...");
+    }
+    
+    if(window.innerWidth < 330) {
+        links.forEach(link => link.innerText = link.innerText.substring(0,20) + "...");
+    }
+    
+})
+
+
+
   
