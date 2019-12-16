0ee95ed89809d9bb648058e55c3a1dafea0652cf
nataliajagus <natalia.jagus@gmail.com>
2019-12-16 13:36:33 +0100

    fix RWD

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ let websiteCardHeaders = document.querySelectorAll('.website-card-header');
 websiteCardHeaders.forEach(header => header.addEventListener("click", (e) => {
     let websiteCard = header.closest(".website-card");
 
-    if(websiteCard.offsetHeight <= 120) {
+    if(websiteCard.offsetHeight <= 140) {
         websiteCard.style.maxHeight = "1000px";
     } else if(window.innerWidth <= 768) {
-        websiteCard.style.maxHeight = "110px";
+        websiteCard.style.maxHeight = "140px";
     } else {
         websiteCard.style.maxHeight = "86px";
     }
